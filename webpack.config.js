97ba32c675cf7e5e788fe3cc31e21e0b8c87f227
whatwewant <tobewhatwewant@gmail.com>
2017-03-30 13:57:00 +0800

    :sparkles:(ServiceWorker Or AppCache) OfflinePlugin Webpack Plugin: @TODO develope use service worker, but production mode use app cache, why ?

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,20 @@
 * @Date:   2017-03-30T11:16:28+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-03-30T11:40:21+08:00
+* @Last modified time: 2017-03-30T13:50:47+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
 
 import OfflinePlugin from 'offline-plugin';
+
 export default (webpackConfig, env) => {
-  webpackConfig.plugins.push(new OfflinePlugin());
+  if (env === 'production') {
+    webpackConfig.plugins.push(new OfflinePlugin({
+      AppCache: {
+        directory: './',
+      },
+    }));
+  }
   return webpackConfig;
-}
+};
