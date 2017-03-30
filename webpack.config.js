605e9940abb990a8c388b74291971d582fa568c8
whatwewant <tobewhatwewant@gmail.com>
2017-03-30 18:31:44 +0800

    :arrow_up: ServiceWorker vs AppCache
    Use OfflinePlugin instead of SW-precache-webpack-plugin, because OfflinePlugin can download service worker to app cache with AppCache setting for not https
    
    Signed-off-by: Eason <uniquecolesmith@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,19 @@
 * @Date:   2017-03-30T11:16:28+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-03-30T15:35:20+08:00
+* @Last modified time: 2017-03-30T18:30:38+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
 
-import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
+import OfflinePlugin from 'offline-plugin';
 
 export default (webpackConfig, env) => {
   if (env === 'production') {
-    webpackConfig.plugins.push(new SWPrecacheWebpackPlugin({
-      filename: 'sw.js',
+    webpackConfig.plugins.push(new OfflinePlugin({
+      AppCache: {
+        directory: './',
+      },
     }));
   }
   return webpackConfig;
