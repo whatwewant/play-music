4df3108bb81a214a4178ca9c9805593cf737fb7a
whatwewant <tobewhatwewant@gmail.com>
2017-04-10 21:32:32 +0800

    :hammer:(OfflinePlugin) solve why develop mode not use service worker, because develop mode cannot acces /sw.js path, if you want test service worker, you need start server from dist/ path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 * @Date:   2017-03-26T00:56:38+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-04-10T18:50:52+08:00
+* @Last modified time: 2017-04-10T21:25:22+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 * @Date:   2017-03-30T11:16:28+08:00
 * @Email:  uniquecolesmith@gmail.com
 * @Last modified by:   eason
-* @Last modified time: 2017-03-30T18:30:38+08:00
+* @Last modified time: 2017-04-10T21:30:55+08:00
 * @License: MIT
 * @Copyright: Eason(uniquecolesmith@gmail.com)
 */
@@ -13,6 +13,9 @@ import OfflinePlugin from 'offline-plugin';
 export default (webpackConfig, env) => {
   if (env === 'production') {
     webpackConfig.plugins.push(new OfflinePlugin({
+      ServiceWorker: {
+        minify: true,
+      },
       AppCache: {
         directory: './',
       },
